24d279c4ca76f73df4efc719842513cb75cd2433
yk0112 <yosikou0726@gmail.com>
2024-06-16 03:20:24 +0900

    add key mapping

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "gopls", "pyright", "tsserver"}
+local servers = { "html", "cssls", "clangd", "gopls", "pyright", "tsserver", "rust_analyzer" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -13,5 +13,3 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
-
-
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,6 +4,26 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- useful bindings
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "kj", "<ESC>")
 map("n", "<c-\\>", ":ToggleTerm<Return>")
+map("n", "<leader>e", ":NvimTreeToggle<cr>")
+
+-- タブの移動
+map("n", "<C-n>", ":bnext<Return>")
+map("n", "<C-p>", ":bprevious<Return>")
+
+--ウィンドウの分割
+map("n", "<C-w>v", ":vsp<cr>")
+map("n", "<C-w>s", ":sp<cr>")
+
+--ウィンドウの移動
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+--ファイルを閉じる
+map("n", "<C-x>", ":bd<cr>")
